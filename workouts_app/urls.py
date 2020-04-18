89927873a2038d682ed7b09a90c51dda4100a49d
Matthew Cockrell <mdc3kw@virginia.edu>
2020-04-18 14:46:28 -0400

    Added new workout and exercise pages

--- workouts_app/urls.py
+++ workouts_app/urls.py
@@ -6,6 +6,12 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    url('', views.HomePageView.as_view(), name='home'),
     path('', views.index, name='index'),
+    path('home/', views.HomePageView.as_view(), name='home'),
+    path('profile/', views.MyProfileView.as_view(), name='profile'),
+    path('new-workout/', views.NewWorkoutView.as_view(), name='new-workout'),
+    path('add-strength-exercise/<int:wid>/', views.AddStrengthExerciseView.as_view(), name='add-strength-exercise'),
+    path('add-cardio-exercise/<int:wid>/', views.AddCardioExerciseView.as_view(), name='add-cardio-exercise'),
+    path('add-hiit-exercise/<int:wid>/', views.AddHIITExerciseView.as_view(), name='add-hiit-exercise'),
+    path('add-exercise/<int:wid>/', views.AddExerciseView.as_view(), name='add-exercise'),
 ]
