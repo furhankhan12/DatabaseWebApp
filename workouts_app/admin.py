3c3cf81efe5d6f3cd135a600be3b9f613ef7efd7
Tanner Dellett-Wion <tdellettwion@gmail.com>
2020-04-18 22:12:23 -0400

    register models

--- workouts_app/admin.py
+++ workouts_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import User, Workout, Location, WorkedOutAt, Exercise, Plan, Cardio, Hiit, Strength, Reps 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Workout)
+admin.site.register(Location)
+admin.site.register(WorkedOutAt)
+admin.site.register(Exercise)
+admin.site.register(Plan)
+admin.site.register(Cardio)
+admin.site.register(Hiit)
+admin.site.register(Strength)
+admin.site.register(Reps)
