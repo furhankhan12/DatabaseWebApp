3bd10ac664c04ab27e3fa69ff57aca717c54e360
Matthew Cockrell <mdc3kw@virginia.edu>
2020-04-25 16:06:14 -0400

    Fixes for models and migrations

--- hoosworkinout/admin.py
+++ hoosworkinout/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Workout, Location, WorkedOutAt, Exercise, Plan, Cardio, Hiit, Strength, Reps 
+from .models import User, Workout, Location, WorkedOutAt, Exercise, Plan, Cardio, Hiit, Strength, Reps
 # Register your models here.
 
 admin.site.register(User)
@@ -7,8 +7,8 @@ admin.site.register(Workout)
 admin.site.register(Location)
 admin.site.register(WorkedOutAt)
 admin.site.register(Exercise)
+admin.site.register(Reps)
 admin.site.register(Plan)
 admin.site.register(Cardio)
 admin.site.register(Hiit)
 admin.site.register(Strength)
-admin.site.register(Reps)
